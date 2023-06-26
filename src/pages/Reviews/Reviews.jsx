b4c8b0217ba79c42c9e3b8d96bb12e171d2662c6
8wondrs <8wondrs@gmail.com>
2023-06-26 23:23:59 +0300

    fix path

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { TheMovieDbAPI } from '../../services/TheMovieDbAPI';
+import { TheMovieDbAPI } from '../../services/api.js';
 import { useParams } from 'react-router-dom';
 import {
   Title,
